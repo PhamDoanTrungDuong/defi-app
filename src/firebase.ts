fbbd3489250cc34135f9784f759fcf6338c89118
thienanga075 <pdtduong@gmail.com>
2022-07-31 16:23:30 +0700

    deploy

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBsHN-739eGDfMqOGe9FHZ6-FDZOSSMHA",
+  authDomain: "defi-app-2d66d.firebaseapp.com",
+  projectId: "defi-app-2d66d",
+  storageBucket: "defi-app-2d66d.appspot.com",
+  messagingSenderId: "904025822258",
+  appId: "1:904025822258:web:1bb2c3959b3615a641d82b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
